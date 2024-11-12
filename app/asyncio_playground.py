91c9e6831eb4857ba466820f6edaed48259c0d3f
Damian Rotta <dipiert@gmail.com>
2024-11-12 09:47:01 -0300

    codecrafters submit [skip ci]

--- app/asyncio_playground.py
+++ app/asyncio_playground.py
@@ -2,16 +2,16 @@ import asyncio
 
 async def redis_server(reader, writer):
     while True:
-        data = await reader.read(100)
+        data = reader.read(100)
         if not data:
             break
         writer.write(b"+PONG\r\n")
         await writer.drain()
     writer.close()
 
-async def main(host, port):
-    server = await asyncio.start_server(redis_server, host, port)
+async def main():
+    server = await asyncio.start_server(redis_server, 'localhost', 6376)
     await server.serve_forever()
     
 if __name__ == '__main__':
-    asyncio.run(main('localhost', 6379))
+    main()
